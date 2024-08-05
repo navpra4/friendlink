dc8ba8c489d22a06aab1ca96f43d00873cd940d4
navpra4 <naveenpra4@gmail.com>
2024-08-05 11:23:15 +0530

    following feed added

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,29 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
-import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default function Home() {
 
-  // const posts = await prisma.post.findMany({
-  //   include: postDataInclude,
-  //   orderBy: {createdAt: 'desc'},
-  // })
+  
   return (
     <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5 ">
         <PostEditor/>
-        <ForYouFeed/>
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed/>
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed/>
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar/>
     </main>
