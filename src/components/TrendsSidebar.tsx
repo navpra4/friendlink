56499159d8e1fca52b421342702427a52d4283b5
navpra4 <naveenpra4@gmail.com>
2024-08-05 18:57:00 +0530

    linkify and tooltip added

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -9,6 +9,7 @@ import { unstable_cache } from "next/cache";
 import { formatNumber } from "@/lib/utils";
 import FollowButton from "./FollowButton";
 import { getUserDataSelect } from "@/lib/types";
+import UserToolTip from "./UserToolTip";
 
 const TrendsSidebar = () => {
 
@@ -50,17 +51,19 @@ async function WhoToFollow() {
             </div>
             {usersToFollow.map(user =>(
                 <div key={user.id} className="flex items-center justify-between gap-3">
-                    <Link href={`/users/${user.username}`} className = 'flex items-center gap-3'>
-                        <UserAvatar avatarUrl={user.avatarUrl} className=" flex-none"/>
-                        <div>
-                            <p className="line-clamp-1 break-all font-semibold hover:underline">
-                                {user.displayName}
-                            </p>
-                            <p className="line-clamp-1 break-all text-muted-foreground">
-                                @{user.username}
-                            </p>
-                        </div>
-                    </Link>
+                    <UserToolTip user={user}> 
+                        <Link href={`/users/${user.username}`} className = 'flex items-center gap-3'>
+                            <UserAvatar avatarUrl={user.avatarUrl} className=" flex-none"/>
+                            <div>
+                                <p className="line-clamp-1 break-all font-semibold hover:underline">
+                                    {user.displayName}
+                                </p>
+                                <p className="line-clamp-1 break-all text-muted-foreground">
+                                    @{user.username}
+                                </p>
+                            </div>
+                        </Link>
+                    </UserToolTip>
                     <FollowButton userId={user.id} initialState={{
                         followers: user._count.followers,
                         isFollowedByUser: user.followers.some(({followerId})=> followerId === user.id),
